2ec690ab4265dfed388c6a1f1e5f5d85dd9e13d4
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-19 10:43:32 +0530

    adding file

---
+++ src/test/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    public TestController() {
+        System.out.println(">>> TestController LOADED <<<");
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Application is running successfully!";
+    }
+}
